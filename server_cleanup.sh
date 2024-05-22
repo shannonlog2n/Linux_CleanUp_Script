4301635d6aff8e58b4d66c8ae1ec71c299b1542d
shannonlog2n <151436075+shannonlog2n@users.noreply.github.com>
2024-05-22 17:06:43 +0800

    Update server_cleanup.sh

--- server_cleanup.sh
+++ server_cleanup.sh
@@ -33,6 +33,7 @@ find /var/log -type f -name "*.log" -exec truncate -s 0 {} \; > /dev/null 2>&1
 find /root -type f -name "*.log" -exec truncate -s 0 {} \; > /dev/null 2>&1
 find /home -type f -name "*.log" -exec truncate -s 0 {} \; > /dev/null 2>&1
 find /ql -type f -name "*.log" -exec truncate -s 0 {} \; > /dev/null 2>&1
+find /var/lib/docker/containers -type f -name "*.log" -exec truncate -s 0 {} \; > /dev/null 2>&1
 
 # 清理缓存目录
 echo "正在清理缓存目录..."
